53e42d58bf74811ce6fe994469934b537b890301
ceejayps <ceejayps1308@gmail.com>
2023-04-09 10:32:37 -0500

    Update auth.js

--- functions/controllers/auth.js
+++ functions/controllers/auth.js
@@ -1,5 +1,18 @@
+const { createUser } = require("./functions/users");
+
 exports.login = (req, res) => {
     const {email,password,username} = req.body;
     console.log(req.body)
     res.status(200).send({email, password, username});
+}
+
+exports.register = (req, res) =>{
+    const {email, password ,username} = req.body;
+    const newUser =createUser({email, password, username})
+    res.send(newUser)
+
+
+
+
+
 }
