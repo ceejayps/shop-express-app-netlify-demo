82fe7fc4c92e069dde36b625c8c95dbe22ce62ea
ceejayps <ceejayps1308@gmail.com>
2023-04-09 10:19:07 -0500

    Create users.js

---
+++ functions/controllers/functions/users.js
@@ -0,0 +1,56 @@
+const { users } = require("../../data/data")
+const bcrypt = require("bcrypt");
+exports.getUserById = (id) =>{
+    const user = users.find(user => user.id === id);
+  return user;
+}
+
+exports.getUserByEmail = (email) =>{
+    const user = users.find(user => user.email === email);
+  return user;
+}
+
+
+exports.getUserByUsername = (username) =>{
+    const user = users.find(user => user.username === username);
+  return user;
+}
+
+exports.getAllUsers = ()=>{
+    return users;
+}
+
+exports.createUser = (newUser)=> {
+
+    // generate a new ID for the user
+    const newUserId = users.length + 1;
+ 
+
+// generate a salt (a random string used in the hashing process)
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+
+// hash a password using the generated salt
+const password = "secret";
+const hashedPassword = bcrypt.hashSync(password, salt);
+  
+    // add the new user to the array
+    users.push({ ...newUser, id: newUserId });
+  
+    // return the new user object
+    return { ...newUser, id: newUserId };
+  }
+
+  exports.updateUser =(id, updatedUserData)=> {
+    // find the user by id
+    const userToUpdate = users.find(user => user.id === id);
+  
+    if (!userToUpdate) {
+      return null; // user not found
+    }
+  
+    // update the user data
+    Object.assign(userToUpdate, updatedUserData);
+  
+    return userToUpdate;
+  }
