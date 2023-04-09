3afe0ebd181d590ea0b8b95dd6e06ebcce76a10e
ceejayps <ceejayps1308@gmail.com>
2023-04-09 10:47:49 -0500

    Update auth.js

--- functions/controllers/auth.js
+++ functions/controllers/auth.js
@@ -1,20 +1,22 @@
 const { createUser, userExists } = require("./functions/users");
 
 exports.login = (req, res) => {
-    const {email,password,username} = req.body;
-    console.log(req.body)
-    res.status(200).send({email, password, username});
-}
+  const { email, password, username } = req.body;
+  console.log(req.body);
+  res.status(200).send({ email, password, username });
+};
 
-exports.register = (req, res) =>{
-    const {email, password ,username} = req.body;
-    if (userExists(email, username)) return res.status(400).json({ message: "User with this email or username already exists" });
-      
-    const newUser =createUser({email, password, username})
-    res.send(newUser)
+exports.register = (req, res) => {
+  const { email, password, username } = req.body;
+  if (userExists(email, ""))
+    return res
+      .status(400)
+      .json({ message: "User with this email already exists" });
+  if (userExists("", username))
+    return res
+      .status(400)
+      .json({ message: "User with this username already exists" });
 
-
-
-
-
-}
+  const newUser = createUser({ email, password, username });
+  res.send(newUser);
+};
